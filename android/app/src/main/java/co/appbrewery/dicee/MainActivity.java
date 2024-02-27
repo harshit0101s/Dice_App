4f547689857f0aac291cfc6de349b8363199d7e7
harshit0101s <147337620+harshit0101s@users.noreply.github.com>
2024-02-27 09:56:11 +0530

    Add files via upload

---
+++ android/app/src/main/java/co/appbrewery/dicee/MainActivity.java
@@ -0,0 +1,17 @@
+package co.appbrewery.dicee;
+
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import android.os.Bundle;
+
+
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  public void configureFlutterEngine(FlutterEngine flutterEngine) {
+    super.configureFlutterEngine(flutterEngine);
+    // Your configuration code here
+  }
+
+
+}
